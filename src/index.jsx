fd1ac970c3c2a1523b51a8ee32b7ca8de924c780
i-johnxz <a824440331@qq.com>
2016-09-29 10:32:09 +0800

    update index

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,7 @@
 import { render } from 'react-dom'
 import { AppRouter } from 'router'
+import { polyfill } from 'es6-promise'
+import fetch from 'isomorphic-fetch'
+polyfill()
 
 render(AppRouter, document.getElementById('app'))
